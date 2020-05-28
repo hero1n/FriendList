4ba18f8a5f201022e708dc7117bfc56ce89c5412
Jaewon Noh <opqw987@gmail.com>
2020-05-28 15:00:44 +0900

    Initial Commit

---
+++ FriendList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FriendList
+//
+//  Created by jaewon on 2020/05/28.
+//  Copyright © 2020 jaewon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
