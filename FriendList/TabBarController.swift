35964525feca63fbd9f1ce274798f976d41a5aa6
Jaewon Noh <opqw987@gmail.com>
2020-05-29 15:55:50 +0900

    Tab bar controllers 설정

--- FriendList/TabBarController.swift
+++ FriendList/TabBarController.swift
@@ -17,5 +17,19 @@ class TabBarController: UITabBarController {
     }
     
     private func configure() {
+        let contactViewController = ContactViewController()
+        contactViewController.tabBarItem = UITabBarItem(title: "연락처", image: UIImage(systemName: "person.crop.circle"), tag: 1)
+
+        let searchViewController  = SearchViewController()
+        searchViewController.tabBarItem = UITabBarItem(title: "국가검색", image: UIImage(systemName: "flag"), tag: 2)
+        
+        let tagViewController = TagViewController()
+        tagViewController.tabBarItem = UITabBarItem(title: "태그", image: UIImage(systemName: "tag"), tag: 3)
+        
+        let settingViewController = SettingViewController()
+        settingViewController.tabBarItem = UITabBarItem(title: "설정", image: UIImage(systemName: "gear"), tag: 4)
+
+        let tabBarList = [contactViewController, searchViewController, tagViewController, settingViewController]
+        self.viewControllers = tabBarList
     }
 }
