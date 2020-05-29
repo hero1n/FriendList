867a65e5afb103e6b2387814c7062cc2e264bf23
Jaewon Noh <opqw987@gmail.com>
2020-05-29 21:22:10 +0900

    UITableViewCell Extension, Debugging.swift Utility 추가

---
+++ FriendList/Support Files/Extensions/UITableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  UITableViewCell.swift
+//  FriendList
+//
+//  Created by jaewon on 2020/05/29.
+//  Copyright © 2020 jaewon. All rights reserved.
+//
+
+import UIKit
+
+extension UITableViewCell {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
+
