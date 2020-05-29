eb9f73d87b3757885ab90d01777b3d27fd5df383
Jaewon Noh <opqw987@gmail.com>
2020-05-29 15:18:42 +0900

    초기 설정

---
+++ FriendList/TabBarController.swift
@@ -0,0 +1,21 @@
+//
+//  TabBarController.swift
+//  FriendList
+//
+//  Created by jaewon on 2020/05/29.
+//  Copyright © 2020 jaewon. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.configure()
+    }
+    
+    private func configure() {
+    }
+}
