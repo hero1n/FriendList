7f2b352ad765d7c1c86fc8a1b02789f6db604a51
Jaewon Noh <opqw987@gmail.com>
2020-05-30 19:22:46 +0900

    Base navigation tint color 수정

--- FriendList/BaseViewController.swift
+++ FriendList/BaseViewController.swift
@@ -24,5 +24,6 @@ class BaseViewController: UIViewController {
     func configureDefaultUI() {
         self.view.backgroundColor = .white
         self.navigationController?.navigationBar.barTintColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        self.navigationController?.navigationBar.tintColor = .white
     }
 }
