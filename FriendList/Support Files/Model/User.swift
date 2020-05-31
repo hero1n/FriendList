da4a886cb04ad1a7299848ca92309aa5bf1b0d22
Jaewon Noh <opqw987@gmail.com>
2020-05-31 19:12:13 +0900

    User model 수정

--- FriendList/Support Files/Model/User.swift
+++ FriendList/Support Files/Model/User.swift
@@ -7,11 +7,30 @@
 //
 
 import Foundation
+import Then
 
-class User {
+class User: NSObject {
     var name: String?
     var phone: String?
     var email: String?
     var country: String?
     var tag: [String]?
+    
+    func toDictionary() -> Dictionary<String, Any?> {
+        return ["name": self.name,
+            "phone": self.phone,
+            "email": self.email,
+            "country": self.country,
+            "tag": self.tag]
+    }
+    
+    static func from(dictionary: Dictionary<String, Any?>) -> User {
+        return User().then {
+            $0.name = dictionary["name"] as? String
+            $0.phone = dictionary["phone"] as? String
+            $0.email = dictionary["email"] as? String
+            $0.country = dictionary["country"] as? String
+            $0.tag = dictionary["tag"] as? [String]
+        }
+    }
 }
