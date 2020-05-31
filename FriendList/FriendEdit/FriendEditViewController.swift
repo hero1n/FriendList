aca3e028c1b365da4e42cbb5d3899d73206f7df1
Jaewon Noh <opqw987@gmail.com>
2020-05-31 17:36:28 +0900

    Friend edit 추가

---
+++ FriendList/FriendEdit/FriendEditViewController.swift
@@ -0,0 +1,22 @@
+//
+//  FriendEditViewController.swift
+//  FriendList
+//
+//  Created by jaewon on 2020/05/31.
+//  Copyright © 2020 jaewon. All rights reserved.
+//
+
+import UIKit
+import Then
+
+class FriendEditViewController: BaseController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.configureUI()
+    }
+    
+    func configureUI() {
+        
+    }
+}
